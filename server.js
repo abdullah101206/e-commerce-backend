5bc8bedffc0f084c77e8a5ce74857505a8daeab2
abdullah101206 <abdullah101206@gmail.com>
2026-09-15 13:00:41 +0500

    Fix CORS origin configuration for Vercel

--- server.js
+++ server.js
@@ -13,13 +13,17 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["https://the-aura-store.vercel.app", "http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors({
-  origin: "*",
-  credentials: true
-}));
 
 app.get("/", (req, res) => {
   res.send("AURA Luxury API is running...");
